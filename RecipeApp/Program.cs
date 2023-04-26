ae6f0145541b35999be765b8a9a0931a289a50a8
Thando Radebe <st10295345@rcconnect.edu.za>
2023-04-26 12:13:06 +0000

    done with the adding of the details onto the array list.

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -22,7 +22,18 @@ namespace RecipeApp
     //the recipe class will collect the data and store it in the array list?? lists??
     class Rescipe 
     {
-    
+        //the two lists and the scaling factor for when the user wants to scale up their recipe
+        private List<Ingredient> _ingredients = new List<Ingredient>();
+        private List<Step> _steps = new List<Step>();
+        private double _scalingFactor = 1.0;
+
+        //adding ingredients to the ingredients array
+        public void AddIngredient(string name, double quantity, string unit)
+        {
+            _ingredients.Add(new Ingredient { Name = name, Quantity = quantity, Unit = unit });
+        }
+
+        //adding ingredients to the steps array
 
 
     }
