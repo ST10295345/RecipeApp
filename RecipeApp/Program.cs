4e882e9b54556ff6f125e74386c7f6189b352a3a
Thando Radebe <st10295345@rcconnect.edu.za>
2023-04-26 12:04:25 +0000

    Decided to just make the ingredient class and step class have varianle only.

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -1,3 +1,23 @@
 ﻿
+using System;
+using System.Collections.Generic;
 
+namespace RecipeApp
+{
+
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+
+    class Step
+    {
+        public int Number { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
 
