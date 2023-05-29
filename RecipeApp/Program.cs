6794d9f6e76bda4a4379d75c94e309073563b2ed
Thando Radebe <st10295345@rcconnect.edu.za>
2023-05-29 11:17:41 +0000

    Changing the recipe app. Found a more efficient way to empliment it.

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -20,67 +20,7 @@ namespace RecipeApp
     }
 
     //the recipe class will collect the data and store it in the array list?? lists??
-    class Recipe 
-    {
-        //the two lists and the scaling factor for when the user wants to scale up their recipe
-        private List<Ingredient> _ingredients = new List<Ingredient>();
-        private List<Step> _steps = new List<Step>();
-        private double _scalingFactor = 1.0;
-
-        //adding ingredients to the ingredients array
-        public void AddIngredient(string name, double amount, string unit)
-        {
-            _ingredients.Add(new Ingredient { Name = name, Amount = amount, Unit = unit });
-        }
-
-        //adding ingredients to the steps array
-        public void AddStep(int number, string description)
-        {
-            _steps.Add(new Step { Number = number, Description = description });
-        }
-
-        //the display of the recipe
-        public void DisplayRecipe()
-        {
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\n*** Recipe ***\n");
-            Console.ResetColor();
-
-            Console.WriteLine("Ingredients:");
-            foreach (var ingredient in _ingredients)
-            {
-                Console.WriteLine($"{ingredient.Amount * _scalingFactor} {ingredient.Unit} {ingredient.Name}");
-            }
-
-            Console.WriteLine("\nSteps:");
-            foreach (var step in _steps)
-            {
-                Console.WriteLine($"{step.Number}. {step.Description}");
-            }
-
-        }
-
-        //now the scalling of the recipe
-        public void ScaleRecipe(double factor)
-        {
-            _scalingFactor = factor;
-        }
-
-        public void ResetScalingFactor()
-        {
-            _scalingFactor = 1.0;
-        }
-
-        //clearing incase the user wants to enter another recipe
-        public void ClearRecipe()
-        {
-            _ingredients.Clear();
-            _steps.Clear();
-            _scalingFactor = 1.0;
-        }
-
-    }
+    
 
 
     class Program
